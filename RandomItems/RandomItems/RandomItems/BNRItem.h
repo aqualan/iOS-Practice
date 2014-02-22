6ddbd383b3987d22eae0914d6f3c5c5e1fa3642b
aqualan <aaquino@seas.upenn.edu>
2014-02-22 16:45:53 -0500

    Finished Chapter 2 work

---
+++ RandomItems/RandomItems/RandomItems/BNRItem.h
@@ -0,0 +1,38 @@
+//
+//  BNRItem.h
+//  RandomItems
+//
+//  Created by Alan Aquino on 2/22/14.
+//  Copyright (c) 2014 Alan Aquino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+{
+    NSString *_itemName;
+    NSString *_serialNumber;
+    int _valueinDollars;
+    NSDate *_dateCreated;
+}
+
++ (instancetype)randomItem;
+
+- (void)setItemName:(NSString *)str;
+- (NSString *)itemName;
+
+- (void)setSerialNumber:(NSString *)str;
+- (NSString *)serialNumber;
+
+- (void)setValueInDollars:(int)v;
+- (int)valueInDollars;
+
+- (NSDate *)dateCreated;
+
+// Designated initializer for BNRItem
+- (instancetype)initWithItemName:(NSString *)name
+                  valueInDollars:(int)value
+                    serialNumber:(NSString *)sNumber;
+
+- (instancetype)initWithItemName:(NSString *)name;
+@end
